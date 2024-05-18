bee267bdf9d07fbaeb954aaeabca4832d9815629
TheFJcurve <sargunbhatti27@gmail.com>
2024-05-18 01:51:50 -0400

    added a frontend, need to add dark mode

---
+++ frontend/pos-blockchain-frontend/src/main.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import theme from "./theme.ts";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
